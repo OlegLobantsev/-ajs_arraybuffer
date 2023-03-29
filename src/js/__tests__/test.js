558a16291bbe8fd5abe2624cb001b7463bb06274
OlegLobantsev <j.legis@gmail.com>
2023-03-29 13:55:42 +0300

    init

---
+++ src/js/__tests__/test.js
@@ -0,0 +1,17 @@
+import getBuffer from '../getBuffer';
+import ArrayBufferConverter from '../app';
+
+test('load()', () => {
+  const arrayBuffer = new ArrayBufferConverter();
+  const buffer = getBuffer();
+  arrayBuffer.load(buffer);
+  expect(arrayBuffer.bufferContainer).toBeInstanceOf(Uint16Array);
+});
+
+test('toString()', () => {
+  const dataStr = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
+  const arrayBuffer = new ArrayBufferConverter();
+  const buffer = getBuffer();
+  arrayBuffer.load(buffer);
+  expect(arrayBuffer.toString()).toBe(dataStr);
+});
